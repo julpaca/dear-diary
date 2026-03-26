ac92fc53129b6118e39fd4617daa30778c2c57ec
julpaca <yulpaca@yandex.ru>
2026-03-26 22:24:04 +0300

    feat (styles): add global styles and delete boilerplated styles

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,8 @@
-<script setup>
+<script setup lang="ts">
 import { RouterView } from 'vue-router'
-import Header from './components/Header.vue'
+import './shared/styles/_global.scss'
 </script>
 
 <template>
-  <Header />
   <RouterView />
 </template>
-
-<style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap');
-
-* { 
-  font-family: "Roboto", sans-serif; 
-  box-sizing: border-box; 
-  padding: 0; 
-  margin: 0; 
-} 
-.container { 
-margin: 0 auto; 
-max-width: 1100px; 
-}
-</style>
