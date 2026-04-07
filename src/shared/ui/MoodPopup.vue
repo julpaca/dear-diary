1f0ee84e95278e25e495c000217ab8616935af27
julpaca <yulpaca@yandex.ru>
2026-04-07 19:11:19 +0300

    feat: add prop value for mood popup

--- src/shared/ui/MoodPopup.vue
+++ src/shared/ui/MoodPopup.vue
@@ -1,14 +1,27 @@
-<script setup>
-
+<script setup lang="ts">
+  const props = defineProps<{ mood: string }>()
 </script>
 
 <template>
-  <div class="popup"></div>
+  <div class="popup">
+    <span>{{ mood }}</span>
+  </div>
 </template>
 
-<style lang="css" scoped>
+<style lang="scss" scoped>
   .popup {
+    width: fit-content;
+    height: fit-content;
+    padding: 1.2rem 1.7rem 0.7rem 1.2rem;
+    font-size: 1.5rem;
+
+    border-radius: 21px 37px 14px 0px;
     color: #A43636;
     background-color: #C5E1E2;
+
+    &:hover {
+      filter: brightness(0.95);
+      transition: filter 0.2s ease-in-out;
+    }
   }
 </style>
