6753c72847a606026c1cc6f8f82349ec906a21dc
julpaca <yulpaca@yandex.ru>
2026-04-11 22:06:08 +0300

    feat: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
+import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
-export default defineConfig({
-  plugins: [vue(), svgLoader({
-    svgoConfig: {
-      multipass: true
+export default defineConfig(({ mode }) => {
+  
+  return {
+    plugins: [vue(), svgLoader()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        'envConfig': fileURLToPath(new URL(`./src${mode}.ts`, import.meta.url))
+      }
     }
-  })],
+  }
 })
